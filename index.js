47f3c5241ec6617a4921146b7c1ac4e69cc3d9ae
Travis Mendez <Travis.mendez779@gmail.com>
2022-09-29 15:48:56 -0700

    commit tree 1d1350a9d3ccb44e520a0d4bcbaf8ed84bfd957c

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+
+}
+function appendCat(name) {
+   const copyCats = [...cats, "Broom"];
+   return copyCats;
+}
+function prependCat(name) {
+    const copyCats2 = ["Arnold", ...cats]
+    return copyCats2;
+}
+function removeLastCat(name) {
+    const removeLastCat = cats.slice(0,2);
+    return removeLastCat;
+}
+function removeFirstCat(name) {
+    const removeFirstCat =cats.slice(1);
+    return removeFirstCat;
+}
